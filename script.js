638bf0a21fd0d7e6f9677a972811cc899d17e6bc
petraalpigiano <petraalpigiano@gmail.com>
2026-01-04 17:00:14 +0100

    add some details

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".fa-solid.fa-bars.hamburger");
 const menuForMobile = document.querySelector(".nav-links");
+const forms = document.querySelector(".contact-form");
+
 hamburger.addEventListener("click", () => {
   console.log(menuForMobile.classList);
   menuForMobile.classList.toggle("display");
   console.log(menuForMobile.classList);
+});
+
+forms.addEventListener("submit", (event) => {
+  event.preventdefault();
 });
